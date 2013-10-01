6c416df5d2fa36a2f8ed5c3df94891208fc25640
Veraxus <matt@mattvanandel.com>
2013-10-01 11:47:26 -0700

    Simplified functionality
    Changed feature set so that plugin is dedicated to REMOVING widgets.
    Now automatically removes the "spam" widgets by default.

--- init.php
+++ init.php
@@ -13,16 +13,25 @@ NV_Configure_Admin_Dashboard::init();
 
 class NV_Configure_Admin_Dashboard {
 
+    /**
+     * Initializes everything...
+     */
     public static function init() {
         add_action( 'wp_dashboard_setup', array( __CLASS__, 'remove_widgets' ) );
-        add_action( 'wp_dashboard_setup', array( __CLASS__, 'add_widget' ) );
     }
 
     /**
-     * Customizes admin dashboard. To all dashboard widget possibilities, add the following the function:
+     * Allows you to completely remove various dashboard widgets (remember they can also be HIDDEN by a user from admin).
+     * To hide a widget, simply UNCOMMENT the appropriate remove_meta_box() line.
+     *
+     * For a complete example of ADDING a widget (which is too involved for this small plugin), see plugin:
+     * "NOUVEAU Dashboard Widget Example"
+     *
+     * To see all dashboard widget possibilities, add the following:
      *
      * {{{
-     * wp_die( '<pre>'.print_r($wp_meta_boxes,true).'</pre>' );
+     * global $wp_meta_boxes;
+     * wp_die( '<pre>'.var_dump($wp_meta_boxes).'</pre>' );
      * }}}
      *
      * Used by hook: 'wp_dashboard_setup'
@@ -32,36 +41,12 @@ class NV_Configure_Admin_Dashboard {
      * @since Nouveau 1.0
      */
     public static function remove_widgets() {
-        //Completely remove various dashboard widgets (remember they can also be HIDDEN by a user from admin)
-
         //remove_meta_box( 'dashboard_quick_press',   'dashboard', 'side' );      //Quick Press widget (quickly write new posts)
         //remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );      //Recent Drafts
-        //remove_meta_box( 'dashboard_primary',       'dashboard', 'side' );      //WordPress.com Blog
-        //remove_meta_box( 'dashboard_secondary',     'dashboard', 'side' );      //Other WordPress News
-        //remove_meta_box( 'dashboard_incoming_links','dashboard', 'normal' );    //Incoming Links
-        //remove_meta_box( 'dashboard_plugins',       'dashboard', 'normal' );    //Plugins
-    }
-
-    /**
-     * Add a widget to the admin dashboard.
-     *
-     * For a complete example of ADDING a widget, see plugin: "NOUVEAU Dashboard Widget Example"
-     *
-     * Used by hook: 'wp_dashboard_setup'
-     *
-     * @see add_action('wp_dashboard_setup',$func)
-     * @link http://codex.wordpress.org/Dashboard_Widgets_API
-     * @since Nouveau 1.0
-     */
-    public static function add_widget() {
-        /*
-        wp_add_dashboard_widget(
-            'unique_slug',                                          //a unique id for the widget
-            __( 'Visible Title', 'nvLangScope' ),                       //Visible name for the widget
-            function(){ require( 'path/to/widget.php' ); },         //Callback for the main widget content
-            function(){ require( 'path/to/widget-config.php' ); }   //Optional callback for widget configuration content
-        );
-        */
+        remove_meta_box( 'dashboard_primary',       'dashboard', 'side' );      //WordPress.com Blog
+        remove_meta_box( 'dashboard_secondary',     'dashboard', 'side' );      //Other WordPress News
+        remove_meta_box( 'dashboard_incoming_links','dashboard', 'normal' );    //Incoming Links
+        remove_meta_box( 'dashboard_plugins',       'dashboard', 'normal' );    //Plugins
     }
 
 }
